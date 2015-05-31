936ad085995c55274cc029f7e8f2165e3a2a9978
BogdanOlar <olar.bogdan.dev@gmail.com>
2015-05-31 18:35:47 -0400

    Initial commit

---
+++ data/clearAutorun.js
@@ -0,0 +1,28 @@
+
+// Make sure we don't override the current onload function
+if(window.onload) {
+    var curronload = window.onload;
+    var newonload = function() {
+        curronload();
+        clickOnAutoplay();
+    };
+    window.onload = newonload;
+} else {
+    window.onload = clickOnAutoplay;
+}
+
+function clickOnAutoplay(){
+    var repetitiveClear = setInterval(
+        function() {
+            var autoplayCheckbox = document.getElementById('autoplay-checkbox');
+            if (autoplayCheckbox != null) {
+                if (autoplayCheckbox.checked) {
+                    autoplayCheckbox.click();
+                } else {
+                    clearInterval(repetitiveClear);
+                }
+            }
+        }
+        , 3000
+    );
+};
